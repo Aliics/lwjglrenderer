5800d82b7e45d0fb94a6fd84bfc90320d8f8bb72
Aliics <Aliics@hotmail.com>
2018-03-05 06:26:12 +1300

    setup a few more classes

---
+++ src/main/java/renderer/Model.java
@@ -0,0 +1,5 @@
+package renderer;
+
+public class Model {
+    private VBORender render;
+}
--- src/main/java/renderer/VBORender.java
+++ src/main/java/renderer/VBORender.java
@@ -1,8 +1,16 @@
 package renderer;
 
+import org.lwjgl.opengl.*;
+
 public class VBORender {
-    private int vbo;
-    private int vao;
     private int vboId;
     private int vaoId;
+
+    public void setVertices(float[] vertices) {
+
+    }
+
+    public void draw() {
+
+    }
 }
